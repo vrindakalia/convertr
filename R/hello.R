1f042993b2f4ec78d4d078b72f8b24fc6800d3e8
Kalia <vk2316@sph-vk2316-pop.local>
2019-04-09 10:54:27 -0400

    Initial commit
    This is a test of linking git to Rstudio. Is the first commit.

---
+++ R/hello.R
@@ -0,0 +1,19 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+c_to_f <- function(temp_in_c){
+    (temp_in_c * 9/5) + 32
+}
+
